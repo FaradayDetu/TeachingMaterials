ca04dfa5e7429c765f90cacc61963d92b172e892
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2017-09-20 09:50:23 +0200

    Course uploaded

--- Master_Modelling/R_course/Introduction_R.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'Introduction_R.Rnw'
-### Encoding: ISO8859-1
-
-###################################################
-### code chunk number 1: setup
-###################################################
-options(width = 80)
-
-
--- Master_Modelling/R_course/creaMaterial.R
+++
@@ -1,5 +0,0 @@
-Sweave("Introduction_R.Rnw")
-shell("pdflatex Introduction_R.tex")
-
-Sweave("lapply.Rnw")
-shell("pdflatex lapply.tex")
